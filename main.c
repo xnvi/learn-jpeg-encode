0d144ff6cece02e4dfcc07e3e95fe27331339dec
bxgj <bwysxl@126.com>
2022-02-01 23:41:31 +0800

    添加一些函数

--- main.c
+++ main.c
@@ -32,9 +32,244 @@ typedef struct __attribute__((packed)) BMP_INFO_HEAD
     unsigned int biClrImportant; // 指定本图象中重要的颜色数，如果该值为零，则认为所有的颜色都是重要的
 }BMP_INFO_HEAD;
 
+
+const uint8_t default_luma_table[] =
+{
+   16, 11, 10, 16,  24,  40,  51,  61,
+   12, 12, 14, 19,  26,  58,  60,  55,
+   14, 13, 16, 24,  40,  57,  69,  56,
+   14, 17, 22, 29,  51,  87,  80,  62,
+   18, 22, 37, 56,  68, 109, 103,  77,
+   24, 35, 55, 64,  81, 104, 113,  92,
+   49, 64, 78, 87, 103, 121, 120, 101,
+   72, 92, 95, 98, 112, 100, 103,  99,
+};
+
+const uint8_t default_chroma_table[] =
+{
+   17, 18, 24, 47, 99, 99, 99, 99,
+   18, 21, 26, 66, 99, 99, 99, 99,
+   24, 26, 56, 99, 99, 99, 99, 99,
+   47, 66, 99, 99, 99, 99, 99, 99,
+   99, 99, 99, 99, 99, 99, 99, 99,
+   99, 99, 99, 99, 99, 99, 99, 99,
+   99, 99, 99, 99, 99, 99, 99, 99,
+   99, 99, 99, 99, 99, 99, 99, 99,
+};
+
+const uint8_t zigzag_table[] =
+{
+    0,   1,  5,  6, 14, 15, 27, 28,
+    2,   4,  7, 13, 16, 26, 29, 42,
+    3,   8, 12, 17, 25, 30, 41, 43,
+    9,  11, 18, 24, 31, 40, 44, 53,
+    10, 19, 23, 32, 39, 45, 52, 54,
+    20, 22, 33, 38, 46, 51, 55, 60,
+    21, 34, 37, 47, 50, 56, 59, 61,
+    35, 36, 48, 49, 57, 58, 62, 63,
+};
 int read_bmp_to_rgb888(char *path, uint8_t **data, int32_t *width, int32_t *height);
 void rgb888_dump_bmp(char *path, uint8_t *data, int32_t width, int32_t height);
 void rgb888_dump_ppm(char *path, uint8_t *data, int32_t width, int32_t height);
+
+int read_block(const uint8_t *rgb, uint8_t *block, int32_t w, int32_t h, int32_t x, int32_t y);
+int block_rgb_to_yuv444(const uint8_t *block, uint8_t *y, uint8_t *u, uint8_t *v);
+int block_dct(const uint8_t *in, double *out);
+int block_quantization(const uint8_t *table, const double *in, int32_t *out);
+int block_zigzag(int32_t *in, int32_t *out);
+int make_qt_table(int32_t qt, const uint8_t *in, uint8_t *out);
+int get_val_bit_and_code(int32_t value, uint16_t *bit_num, uint16_t *code);
+
+double ck(int k)
+{
+    if (k == 0)
+    {
+        return sqrt(1.0 / DCT_SIZE);
+    }
+    else
+    {
+        return sqrt(2.0 / DCT_SIZE);
+    }
+}
+
+int block_dct(const uint8_t *in, double *out)
+{
+    int i, j, n, m;
+    double sum = 0.0;
+
+    for (m = 0; m < DCT_SIZE; m++)
+    {
+        for (n = 0; n < DCT_SIZE; n++)
+        {
+            for (i = 0; i < DCT_SIZE; i++)
+            {
+                for (j = 0; j < DCT_SIZE; j++)
+                {
+                    // sum += in[i][j] * cos((2 * j + 1) * n * M_PI / (2 * DCT_SIZE)) * cos((2 * i + 1) * m * M_PI / (2 * DCT_SIZE));
+                    sum += in[i * DCT_SIZE + j] * cos((2 * j + 1) * n * M_PI / (2 * DCT_SIZE)) * cos((2 * i + 1) * m * M_PI / (2 * DCT_SIZE));
+                }
+            }
+            // out[m][n] = sum * ck(m) * ck(n);
+            out[m * DCT_SIZE + n] =  sum * ck(m) * ck(n);
+            sum = 0.0;
+        }
+    }
+
+    return 0;
+}
+
+int read_block(const uint8_t *rgb, uint8_t *block, int32_t w, int32_t h, int32_t x, int32_t y)
+{
+    // 长宽不足8可以填充，也可以复制边缘像素
+    int32_t dx, dy;
+    int32_t rgb_offset, block_offset;
+
+    for (dy = 0; dy < 8; dy++)
+    {
+        for (dx = 0; dx < 8; dx++)
+        {
+            rgb_offset = (((y + dy) * w) + (x + dx)) * 3;
+            block_offset = (dy * DCT_SIZE + dx) * 3;
+            // printf("b %d \n", block_offset);
+
+            if (x + dx >= w)
+            {
+                block[block_offset + 0] = block[block_offset - 3 + 0];
+                block[block_offset + 1] = block[block_offset - 3 + 1];
+                block[block_offset + 2] = block[block_offset - 3 + 2];
+                continue;
+            }
+            if (y + dy >= h)
+            {
+                block[block_offset + 0] = block[block_offset - 3 * DCT_SIZE + 0];
+                block[block_offset + 1] = block[block_offset - 3 * DCT_SIZE + 1];
+                block[block_offset + 2] = block[block_offset - 3 * DCT_SIZE + 2];
+                continue;
+            }
+            block[block_offset + 0] = rgb[rgb_offset + 0];
+            block[block_offset + 1] = rgb[rgb_offset + 1];
+            block[block_offset + 2] = rgb[rgb_offset + 2];
+        }
+    }
+
+    return 0;
+}
+
+// YUV有BT601、BT709、BT2020
+// jpeg 使用的yuv公式 https://en.wikipedia.org/wiki/YCbCr
+// 参考JFIF标准
+int block_rgb_to_yuv444(const uint8_t *block, uint8_t *y, uint8_t *u, uint8_t *v)
+{
+    uint8_t r, g, b;
+    int32_t dx, dy;
+    int32_t rgb_offset, block_offset;
+    float luma, cb, cr;
+
+    for (dy = 0; dy < DCT_SIZE; dy++)
+    {
+        for (dx = 0; dx < DCT_SIZE; dx++)
+        {
+            block_offset = (dy * DCT_SIZE + dx);
+            r = block[block_offset * 3 + 0];
+            g = block[block_offset * 3 + 1];
+            b = block[block_offset * 3 + 2];
+            luma = 0.299f   * r + 0.587f  * g + 0.114f  * b;
+            cb   = -0.1687f * r - 0.3313f * g + 0.5f    * b + 128.0f;
+            cr   = 0.5f     * r - 0.4187f * g - 0.0813f * b + 128.0f;
+            y[block_offset] = (uint8_t)luma;
+            u[block_offset] = (uint8_t)cb;
+            v[block_offset] = (uint8_t)cr;
+        }
+    }
+    return 0;
+}
+
+int make_qt_table(int32_t qt, const uint8_t *in, uint8_t *out)
+{
+    int32_t dx, dy;
+    int32_t offset;
+    float alpha;
+    float tmp;
+
+    if (qt < 50) {
+        alpha = 50.0f / qt;
+    }
+    else {
+        alpha = 2.0f - qt / 50.0f;
+    }
+
+    for (dy = 0; dy < DCT_SIZE; dy++)
+    {
+        for (dx = 0; dx < DCT_SIZE; dx++)
+        {
+            offset = dy * DCT_SIZE + dx;
+            tmp = in[offset] * alpha;
+            tmp = tmp < 1 ? 1 : tmp;
+            tmp = tmp > 255 ? 255 : tmp;
+            out[offset] = (uint8_t)tmp;
+        }
+    }
+    return 0;
+}
+
+int block_quantization(const uint8_t *table, const double *in, int32_t *out)
+{
+    int32_t dx, dy;
+    int32_t offset;
+
+    for (dy = 0; dy < DCT_SIZE; dy++)
+    {
+        for (dx = 0; dx < DCT_SIZE; dx++)
+        {
+            offset = dy * DCT_SIZE + dx;
+            out[offset] = round(in[offset] / table[offset]);
+        }
+    }
+    return 0;
+}
+
+int block_zigzag(int32_t *in, int32_t *out)
+{
+    int32_t i;
+    for (i = 0; i < DCT_SIZE * DCT_SIZE; i++)
+    {
+        // out[i] = in[zigzag_table[i]];
+        out[zigzag_table[i]] = in[i];
+    }
+}
+
+int get_val_bit_and_code(int32_t value, uint16_t *bit_num, uint16_t *code)
+{
+    // 计算方法：正数不变，负数则计算它绝对值的反码
+    // 位数为这个数的绝对值的位数
+
+    // 优化方法，利用补码特性
+    int32_t abs_val = value;
+    if ( value < 0 ) {
+        abs_val = -abs_val;
+        value -= 1;
+    }
+    *bit_num = 1;
+    while (abs_val >>= 1) {
+        *bit_num += 1;
+    }
+    *code = (uint16_t)(value & ((1 << *bit_num) - 1));
+
+#if 0
+    // 原始方法
+    int32_t tmp_val = value;
+    int32_t abs_val = value;
+    if (value < 0) {
+        abs_val = -value;
+        tmp_val = ~abs_val;
+    }
+    *bit_num = 1;
+    while (abs_val >>= 1) {
+        *bit_num += 1;
+    }
+    *code = (uint16_t)(tmp_val & ((1 << *bit_num) - 1));
+#endif
+}
 
 int read_bmp_to_rgb888(char *path, uint8_t **data, int32_t *width, int32_t *height)
 {
@@ -164,7 +399,7 @@ end:
     return 1;
 }
 
-void dbg_rgb888_dump_bmp(char *path, uint8_t *data, int32_t width, int32_t height)
+void rgb888_dump_bmp(char *path, uint8_t *data, int32_t width, int32_t height)
 {
     BMP_FILE_HEAD bfh;
     BMP_INFO_HEAD bih;
@@ -224,7 +459,7 @@ void dbg_rgb888_dump_bmp(char *path, uint8_t *data, int32_t width, int32_t heigh
     fclose(fp);
 }
 
-void dbg_rgb888_dump_ppm(char *path, uint8_t *data, int32_t width, int32_t height)
+void rgb888_dump_ppm(char *path, uint8_t *data, int32_t width, int32_t height)
 {
     FILE *fp;
     char ppm_head[128];
