20edaeba3d2a6da98bc0453e61e192d321d713b6
bxgj <bwysxl@126.com>
2022-02-03 20:37:52 +0800

    完善一些函数

--- main.c
+++ main.c
@@ -5,56 +5,60 @@
 #include <math.h>
 
 #define DCT_SIZE 8
+#define HUFF_TREE_DC_LEN 16
+#define HUFF_TREE_AC_LEN 256
+
+FILE *fp_jpeg = NULL;
 
 // 位图文件头
 typedef struct __attribute__((packed)) BMP_FILE_HEAD
 {
-    unsigned short bfType; // 文件类型，必须是0x424D，也就是字符BM
-    unsigned int bfSize; // 文件大小，包含头
-    unsigned short bfReserved1; // 保留字
-    unsigned short bfReserved2; // 保留字
-    unsigned int bfOffBits; // 文件头到实际的图像数据的偏移字节数
+    uint16_t bfType; // 文件类型，必须是0x424D，也就是字符BM
+    uint32_t bfSize; // 文件大小，包含头
+    uint16_t bfReserved1; // 保留字
+    uint16_t bfReserved2; // 保留字
+    uint32_t bfOffBits; // 文件头到实际的图像数据的偏移字节数
 }BMP_FILE_HEAD;
 
 // 位图信息头
 typedef struct __attribute__((packed)) BMP_INFO_HEAD
 {
-    unsigned int biSize; // 这个结构体的长度，为40字节
-    int biWidth; // 图像的宽度
-    int biHeight; // 图像的长度
-    unsigned short biPlanes; // 必须是1
-    unsigned short biBitCount; // 表示颜色时要用到的位数，常用的值为 1（黑白二色图）,4（16 色图）,8（256 色）,24（真彩色图）（新的.bmp 格式支持 32 位色，这里不做讨论）
-    unsigned int biCompression; // 指定位图是否压缩，有效的值为 BI_RGB，BI_RLE8，BI_RLE4，BI_BITFIELDS（都是一些Windows定义好的常量，暂时只考虑BI_RGB不压缩的情况）
-    unsigned int biSizeImage; // 指定实际的位图数据占用的字节数
-    int biXPelsPerMeter; // 指定目标设备的水平分辨率
-    int biYPelsPerMeter; // 指定目标设备的垂直分辨率
-    unsigned int biClrUsed; // 指定本图象实际用到的颜色数，如果该值为零，则用到的颜色数为 2 的 biBitCount 次方
-    unsigned int biClrImportant; // 指定本图象中重要的颜色数，如果该值为零，则认为所有的颜色都是重要的
+    uint32_t biSize; // 这个结构体的长度，为40字节
+    int32_t biWidth; // 图像的宽度
+    int32_t biHeight; // 图像的长度
+    uint16_t biPlanes; // 必须是1
+    uint16_t biBitCount; // 表示颜色时要用到的位数，常用的值为 1（黑白二色图）,4（16 色图）,8（256 色）,24（真彩色图）（新的.bmp 格式支持 32 位色，这里不做讨论）
+    uint32_t biCompression; // 指定位图是否压缩，有效的值为 BI_RGB，BI_RLE8，BI_RLE4，BI_BITFIELDS（都是一些Windows定义好的常量，暂时只考虑BI_RGB不压缩的情况）
+    uint32_t biSizeImage; // 指定实际的位图数据占用的字节数
+    int32_t biXPelsPerMeter; // 指定目标设备的水平分辨率
+    int32_t biYPelsPerMeter; // 指定目标设备的垂直分辨率
+    int32_t biClrUsed; // 指定本图象实际用到的颜色数，如果该值为零，则用到的颜色数为 2 的 biBitCount 次方
+    int32_t biClrImportant; // 指定本图象中重要的颜色数，如果该值为零，则认为所有的颜色都是重要的
 }BMP_INFO_HEAD;
 
 
 const uint8_t default_luma_table[] =
 {
-   16, 11, 10, 16,  24,  40,  51,  61,
-   12, 12, 14, 19,  26,  58,  60,  55,
-   14, 13, 16, 24,  40,  57,  69,  56,
-   14, 17, 22, 29,  51,  87,  80,  62,
-   18, 22, 37, 56,  68, 109, 103,  77,
-   24, 35, 55, 64,  81, 104, 113,  92,
-   49, 64, 78, 87, 103, 121, 120, 101,
-   72, 92, 95, 98, 112, 100, 103,  99,
+    16, 11, 10, 16,  24,  40,  51,  61,
+    12, 12, 14, 19,  26,  58,  60,  55,
+    14, 13, 16, 24,  40,  57,  69,  56,
+    14, 17, 22, 29,  51,  87,  80,  62,
+    18, 22, 37, 56,  68, 109, 103,  77,
+    24, 35, 55, 64,  81, 104, 113,  92,
+    49, 64, 78, 87, 103, 121, 120, 101,
+    72, 92, 95, 98, 112, 100, 103,  99,
 };
 
 const uint8_t default_chroma_table[] =
 {
-   17, 18, 24, 47, 99, 99, 99, 99,
-   18, 21, 26, 66, 99, 99, 99, 99,
-   24, 26, 56, 99, 99, 99, 99, 99,
-   47, 66, 99, 99, 99, 99, 99, 99,
-   99, 99, 99, 99, 99, 99, 99, 99,
-   99, 99, 99, 99, 99, 99, 99, 99,
-   99, 99, 99, 99, 99, 99, 99, 99,
-   99, 99, 99, 99, 99, 99, 99, 99,
+    17, 18, 24, 47, 99, 99, 99, 99,
+    18, 21, 26, 66, 99, 99, 99, 99,
+    24, 26, 56, 99, 99, 99, 99, 99,
+    47, 66, 99, 99, 99, 99, 99, 99,
+    99, 99, 99, 99, 99, 99, 99, 99,
+    99, 99, 99, 99, 99, 99, 99, 99,
+    99, 99, 99, 99, 99, 99, 99, 99,
+    99, 99, 99, 99, 99, 99, 99, 99,
 };
 
 const uint8_t zigzag_table[] =
@@ -68,19 +72,147 @@ const uint8_t zigzag_table[] =
     21, 34, 37, 47, 50, 56, 59, 61,
     35, 36, 48, 49, 57, 58, 62, 63,
 };
-int read_bmp_to_rgb888(char *path, uint8_t **data, int32_t *width, int32_t *height);
+
+const uint8_t default_ht_luma_dc_len[16] =
+{
+    0,1,5,1,1,1,1,1,1,0,0,0,0,0,0,0
+};
+
+const uint8_t default_ht_luma_dc[12] =
+{
+    0,1,2,3,4,5,6,7,8,9,10,11
+};
+
+const uint8_t default_ht_chroma_dc_len[16] =
+{
+    0,3,1,1,1,1,1,1,1,1,1,0,0,0,0,0
+};
+
+const uint8_t default_ht_chroma_dc[12] =
+{
+    0,1,2,3,4,5,6,7,8,9,10,11
+};
+
+const uint8_t default_ht_luma_ac_len[16] =
+{
+    0,2,1,3,3,2,4,3,5,5,4,4,0,0,1,0x7d
+};
+
+const uint8_t default_ht_luma_ac[] =
+{
+    0x01, 0x02, 0x03, 0x00, 0x04, 0x11, 0x05, 0x12, 0x21, 0x31, 0x41, 0x06, 0x13, 0x51, 0x61, 0x07,
+    0x22, 0x71, 0x14, 0x32, 0x81, 0x91, 0xA1, 0x08, 0x23, 0x42, 0xB1, 0xC1, 0x15, 0x52, 0xD1, 0xF0,
+    0x24, 0x33, 0x62, 0x72, 0x82, 0x09, 0x0A, 0x16, 0x17, 0x18, 0x19, 0x1A, 0x25, 0x26, 0x27, 0x28,
+    0x29, 0x2A, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x3A, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48, 0x49,
+    0x4A, 0x53, 0x54, 0x55, 0x56, 0x57, 0x58, 0x59, 0x5A, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68, 0x69,
+    0x6A, 0x73, 0x74, 0x75, 0x76, 0x77, 0x78, 0x79, 0x7A, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89,
+    0x8A, 0x92, 0x93, 0x94, 0x95, 0x96, 0x97, 0x98, 0x99, 0x9A, 0xA2, 0xA3, 0xA4, 0xA5, 0xA6, 0xA7,
+    0xA8, 0xA9, 0xAA, 0xB2, 0xB3, 0xB4, 0xB5, 0xB6, 0xB7, 0xB8, 0xB9, 0xBA, 0xC2, 0xC3, 0xC4, 0xC5,
+    0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xD2, 0xD3, 0xD4, 0xD5, 0xD6, 0xD7, 0xD8, 0xD9, 0xDA, 0xE1, 0xE2,
+    0xE3, 0xE4, 0xE5, 0xE6, 0xE7, 0xE8, 0xE9, 0xEA, 0xF1, 0xF2, 0xF3, 0xF4, 0xF5, 0xF6, 0xF7, 0xF8,
+    0xF9, 0xFA
+};
+
+const uint8_t default_ht_chroma_ac_len[16] =
+{
+    0,2,1,2,4,4,3,4,7,5,4,4,0,1,2,0x77
+};
+
+const uint8_t default_ht_chroma_ac[] =
+{
+    0x00, 0x01, 0x02, 0x03, 0x11, 0x04, 0x05, 0x21, 0x31, 0x06, 0x12, 0x41, 0x51, 0x07, 0x61, 0x71,
+    0x13, 0x22, 0x32, 0x81, 0x08, 0x14, 0x42, 0x91, 0xA1, 0xB1, 0xC1, 0x09, 0x23, 0x33, 0x52, 0xF0,
+    0x15, 0x62, 0x72, 0xD1, 0x0A, 0x16, 0x24, 0x34, 0xE1, 0x25, 0xF1, 0x17, 0x18, 0x19, 0x1A, 0x26,
+    0x27, 0x28, 0x29, 0x2A, 0x35, 0x36, 0x37, 0x38, 0x39, 0x3A, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48,
+    0x49, 0x4A, 0x53, 0x54, 0x55, 0x56, 0x57, 0x58, 0x59, 0x5A, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68,
+    0x69, 0x6A, 0x73, 0x74, 0x75, 0x76, 0x77, 0x78, 0x79, 0x7A, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87,
+    0x88, 0x89, 0x8A, 0x92, 0x93, 0x94, 0x95, 0x96, 0x97, 0x98, 0x99, 0x9A, 0xA2, 0xA3, 0xA4, 0xA5,
+    0xA6, 0xA7, 0xA8, 0xA9, 0xAA, 0xB2, 0xB3, 0xB4, 0xB5, 0xB6, 0xB7, 0xB8, 0xB9, 0xBA, 0xC2, 0xC3,
+    0xC4, 0xC5, 0xC6, 0xC7, 0xC8, 0xC9, 0xCA, 0xD2, 0xD3, 0xD4, 0xD5, 0xD6, 0xD7, 0xD8, 0xD9, 0xDA,
+    0xE2, 0xE3, 0xE4, 0xE5, 0xE6, 0xE7, 0xE8, 0xE9, 0xEA, 0xF2, 0xF3, 0xF4, 0xF5, 0xF6, 0xF7, 0xF8,
+    0xF9, 0xFA
+};
+
+
+typedef struct
+{
+    uint8_t raw;
+    uint8_t code_length;
+    uint16_t code_word;
+} coefficients;
+
+typedef struct __attribute__((packed))
+{
+    uint16_t len;
+    uint8_t  identifier[5];
+    uint16_t version;
+    uint8_t  units;
+    uint16_t Hdensity;
+    uint16_t Vdensity;
+    uint8_t  HthumbnailA;
+    uint8_t  VthumbnailA;
+} JFIF_Header;
+
+
+int32_t read_bmp_to_rgb888(char *path, uint8_t **data, int32_t *width, int32_t *height);
 void rgb888_dump_bmp(char *path, uint8_t *data, int32_t width, int32_t height);
 void rgb888_dump_ppm(char *path, uint8_t *data, int32_t width, int32_t height);
 
-int read_block(const uint8_t *rgb, uint8_t *block, int32_t w, int32_t h, int32_t x, int32_t y);
-int block_rgb_to_yuv444(const uint8_t *block, uint8_t *y, uint8_t *u, uint8_t *v);
-int block_dct(const uint8_t *in, double *out);
-int block_quantization(const uint8_t *table, const double *in, int32_t *out);
-int block_zigzag(int32_t *in, int32_t *out);
-int make_qt_table(int32_t qt, const uint8_t *in, uint8_t *out);
-int get_val_bit_and_code(int32_t value, uint16_t *bit_num, uint16_t *code);
+int32_t read_block(const uint8_t *rgb, uint8_t *block, int32_t w, int32_t h, int32_t x, int32_t y);
+int32_t block_rgb_to_yuv444(const uint8_t *block, uint8_t *y, uint8_t *u, uint8_t *v);
+int32_t block_dct(const uint8_t *in, double *out);
+int32_t block_quantization(const uint8_t *table, const double *in, int32_t *out);
+int32_t block_zigzag(int32_t *in, int32_t *out);
+int32_t huffman_encode();
+int32_t make_qt_table(int32_t qt, const uint8_t *in, uint8_t *out);
+int32_t make_huffman_tree(uint8_t *code_length, uint8_t *raw_val, int32_t out_len, coefficients *out);
+int32_t jpeg_write_head();
+int32_t jpeg_write_data();
+int32_t jpeg_write_end();
 
-double ck(int k)
+int32_t jpeg_write_file(uint8_t *data, int32_t len);
+int32_t jpeg_write_bits(uint32_t data, int32_t len);
+int32_t jpeg_write_u8(uint8_t data);
+int32_t jpeg_write_u16(uint16_t data);
+int32_t jpeg_write_u32(uint32_t data);
+
+int32_t jpeg_write_file(uint8_t *data, int32_t len)
+{
+    return fwrite(data, 1, len, fp_jpeg);
+}
+
+int32_t jpeg_write_bits(uint32_t data, int32_t len)
+{
+    // TODO
+    return 0;
+}
+
+int32_t jpeg_write_u8(uint8_t data)
+{
+    uint8_t wd = data;
+    return fwrite(&wd, 1, sizeof(uint8_t), fp_jpeg);
+}
+
+int32_t jpeg_write_u16(uint16_t data)
+{
+    uint16_t wd = data;
+    return fwrite(&wd, 1, sizeof(uint16_t), fp_jpeg);
+}
+
+int32_t jpeg_write_u32(uint32_t data)
+{
+    uint32_t wd = data;
+    return fwrite(&wd, 1, sizeof(uint32_t), fp_jpeg);
+}
+
+uint16_t sw16(uint16_t dat)
+{
+    uint16_t lo = (dat & 0x00ff);
+    uint16_t hi = ((dat & 0xff00) >> 8);
+    return (uint16_t)((lo << 8) | hi);
+}
+
+double ck(int32_t k)
 {
     if (k == 0)
     {
@@ -92,9 +224,9 @@ double ck(int k)
     }
 }
 
-int block_dct(const uint8_t *in, double *out)
+int32_t block_dct(const uint8_t *in, double *out)
 {
-    int i, j, n, m;
+    int32_t i, j, n, m;
     double sum = 0.0;
 
     for (m = 0; m < DCT_SIZE; m++)
@@ -118,7 +250,7 @@ int block_dct(const uint8_t *in, double *out)
     return 0;
 }
 
-int read_block(const uint8_t *rgb, uint8_t *block, int32_t w, int32_t h, int32_t x, int32_t y)
+int32_t read_block(const uint8_t *rgb, uint8_t *block, int32_t w, int32_t h, int32_t x, int32_t y)
 {
     // 长宽不足8可以填充，也可以复制边缘像素
     int32_t dx, dy;
@@ -158,7 +290,7 @@ int read_block(const uint8_t *rgb, uint8_t *block, int32_t w, int32_t h, int32_t
 // YUV有BT601、BT709、BT2020
 // jpeg 使用的yuv公式 https://en.wikipedia.org/wiki/YCbCr
 // 参考JFIF标准
-int block_rgb_to_yuv444(const uint8_t *block, uint8_t *y, uint8_t *u, uint8_t *v)
+int32_t block_rgb_to_yuv444(const uint8_t *block, uint8_t *y, uint8_t *u, uint8_t *v)
 {
     uint8_t r, g, b;
     int32_t dx, dy;
@@ -184,17 +316,19 @@ int block_rgb_to_yuv444(const uint8_t *block, uint8_t *y, uint8_t *u, uint8_t *v
     return 0;
 }
 
-int make_qt_table(int32_t qt, const uint8_t *in, uint8_t *out)
+int32_t make_qt_table(int32_t qt, const uint8_t *in, uint8_t *out)
 {
     int32_t dx, dy;
     int32_t offset;
     float alpha;
     float tmp;
 
-    if (qt < 50) {
+    if (qt < 50)
+    {
         alpha = 50.0f / qt;
     }
-    else {
+    else
+    {
         alpha = 2.0f - qt / 50.0f;
     }
 
@@ -212,7 +346,44 @@ int make_qt_table(int32_t qt, const uint8_t *in, uint8_t *out)
     return 0;
 }
 
-int block_quantization(const uint8_t *table, const double *in, int32_t *out)
+int32_t make_huffman_tree(uint8_t *code_length, uint8_t *raw_val, int32_t out_len, coefficients *out)
+{
+    int32_t i, j;
+    uint32_t code = 0;
+    int32_t code_bits = 1;
+    int32_t count = 0;
+
+    for(i = 0; i < 16; i++)
+    {
+        for (j = 0; j < code_length[i]; j++)
+        {
+            // 生成
+            // out[count].code_word = code;
+            // out[count].code_length = code_bits;
+            // out[count].raw = raw_val[count];
+
+            // 生成并排序
+            out[raw_val[count]].raw = raw_val[count];
+            out[raw_val[count]].code_word = code;
+            out[raw_val[count]].code_length = code_bits;
+            // printf("num %d, raw %#02x, code %#02x, len %d \n", count, raw_val[count], code, code_bits);
+
+            count += 1;
+            code += 1;
+        }
+        code_bits += 1;
+        code <<= 1;
+    }
+
+    // for (i = 0; i < out_len; i++)
+    // {
+    // 	printf("num %d, raw %#02x, code %#02x, len %d \n", i, out[i].raw, out[i].code_word, out[i].code_length);
+    // }
+
+    return 0;
+}
+
+int32_t block_quantization(const uint8_t *table, const double *in, int32_t *out)
 {
     int32_t dx, dy;
     int32_t offset;
@@ -228,7 +399,7 @@ int block_quantization(const uint8_t *table, const double *in, int32_t *out)
     return 0;
 }
 
-int block_zigzag(int32_t *in, int32_t *out)
+int32_t block_zigzag(int32_t *in, int32_t *out)
 {
     int32_t i;
     for (i = 0; i < DCT_SIZE * DCT_SIZE; i++)
@@ -238,19 +409,21 @@ int block_zigzag(int32_t *in, int32_t *out)
     }
 }
 
-int get_val_bit_and_code(int32_t value, uint16_t *bit_num, uint16_t *code)
+int32_t get_val_bit_and_code(int32_t value, uint16_t *bit_num, uint16_t *code)
 {
     // 计算方法：正数不变，负数则计算它绝对值的反码
     // 位数为这个数的绝对值的位数
 
     // 优化方法，利用补码特性
     int32_t abs_val = value;
-    if ( value < 0 ) {
+    if ( value < 0 )
+    {
         abs_val = -abs_val;
         value -= 1;
     }
     *bit_num = 1;
-    while (abs_val >>= 1) {
+    while (abs_val >>= 1)
+    {
         *bit_num += 1;
     }
     *code = (uint16_t)(value & ((1 << *bit_num) - 1));
@@ -259,34 +432,37 @@ int get_val_bit_and_code(int32_t value, uint16_t *bit_num, uint16_t *code)
     // 原始方法
     int32_t tmp_val = value;
     int32_t abs_val = value;
-    if (value < 0) {
+    if (value < 0)
+    {
         abs_val = -value;
         tmp_val = ~abs_val;
     }
     *bit_num = 1;
-    while (abs_val >>= 1) {
+    while (abs_val >>= 1)
+    {
         *bit_num += 1;
     }
     *code = (uint16_t)(tmp_val & ((1 << *bit_num) - 1));
 #endif
 }
 
-int read_bmp_to_rgb888(char *path, uint8_t **data, int32_t *width, int32_t *height)
+int32_t read_bmp_to_rgb888(char *path, uint8_t **data, int32_t *width, int32_t *height)
 {
     FILE *img_fp = NULL;
     BMP_FILE_HEAD BFH;
     BMP_INFO_HEAD BIH;
-    int file_size = 0;
-    int ret = 0;
+    int32_t file_size = 0;
+    int32_t ret = 0;
     int32_t i = 0, j = 0;
     int32_t h = 0, v = 0;
-    unsigned int offset = 0;
-    unsigned int line_size = 0;
-    unsigned char *bmp_data = NULL;
-    unsigned char *rgb888_data = NULL;
+    uint32_t offset = 0;
+    uint32_t line_size = 0;
+    uint8_t *bmp_data = NULL;
+    uint8_t *rgb888_data = NULL;
 
     img_fp = fopen(path, "rb");
-    if (img_fp == NULL) {
+    if (img_fp == NULL)
+    {
         printf("can not open %s\n", path);
         *data = NULL;
         *width = 0;
@@ -298,7 +474,8 @@ int read_bmp_to_rgb888(char *path, uint8_t **data, int32_t *width, int32_t *heig
     file_size = ftell(img_fp);
     fseek(img_fp, 0, SEEK_SET);
 
-    if (file_size < 54) {
+    if (file_size < 54)
+    {
         printf("file %s size error\n", path);
         *data = NULL;
         *width = 0;
@@ -344,7 +521,8 @@ int read_bmp_to_rgb888(char *path, uint8_t **data, int32_t *width, int32_t *heig
     }
 
     bmp_data = (unsigned char *)malloc(BIH.biSizeImage);
-    if (bmp_data == NULL) {
+    if (bmp_data == NULL)
+    {
         printf("malloc bmp_buf error\n");
         *data = NULL;
         *width = 0;
@@ -355,7 +533,8 @@ int read_bmp_to_rgb888(char *path, uint8_t **data, int32_t *width, int32_t *heig
     // ret = fread(bmp_data, BIH.biSizeImage, 1, img_fp);
     // if (ret != 1) {
     ret = fread(bmp_data, 1, BIH.biSizeImage, img_fp);
-    if (ret != BIH.biSizeImage) {
+    if (ret != BIH.biSizeImage)
+    {
         printf("read bmp file error\n");
         *data = NULL;
         *width = 0;
@@ -365,7 +544,8 @@ int read_bmp_to_rgb888(char *path, uint8_t **data, int32_t *width, int32_t *heig
     fclose(img_fp);
 
     rgb888_data = (unsigned char *)malloc(BIH.biWidth * BIH.biHeight * 3);
-    if (rgb888_data == NULL) {
+    if (rgb888_data == NULL)
+    {
         printf("malloc rgb_buf error\n");
         *data = NULL;
         *width = 0;
@@ -482,16 +662,102 @@ void rgb888_dump_ppm(char *path, uint8_t *data, int32_t width, int32_t height)
     fclose(fp);
 }
 
+void print_block_u8(uint8_t *in)
+{
+    int32_t i, j;
+    printf("\n");
+    for (i = 0; i < DCT_SIZE; i++)
+    {
+        for (j = 0; j < DCT_SIZE; j++)
+        {
+            printf("%3d ", in[i * DCT_SIZE + j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
 int main(int argc, const char **argv)
 {
-    uint8_t *rgb_data;
+    JFIF_Header jfif;
+
     int32_t w, h;
+    int32_t qt = 90;
+
+    uint8_t *rgb_data;
+    uint8_t rgb_block[DCT_SIZE * DCT_SIZE * 3];
+    uint8_t y_block[DCT_SIZE * DCT_SIZE];
+    uint8_t u_block[DCT_SIZE * DCT_SIZE];
+    uint8_t v_block[DCT_SIZE * DCT_SIZE];
+    double dct_block[DCT_SIZE * DCT_SIZE];
+    int32_t qt_block[DCT_SIZE * DCT_SIZE];
+    int32_t zigzag_block[DCT_SIZE * DCT_SIZE];
+    uint8_t luma_table[DCT_SIZE * DCT_SIZE];
+    uint8_t chroma_table[DCT_SIZE * DCT_SIZE];
+
+    coefficients huff_tree_luma_dc[HUFF_TREE_DC_LEN];
+    coefficients huff_tree_chroma_dc[HUFF_TREE_DC_LEN];
+    coefficients huff_tree_luma_ac[HUFF_TREE_AC_LEN];
+    coefficients huff_tree_chroma_ac[HUFF_TREE_AC_LEN];
+    memset(huff_tree_luma_dc,   0, sizeof(huff_tree_luma_dc));
+    memset(huff_tree_chroma_dc, 0, sizeof(huff_tree_chroma_dc));
+    memset(huff_tree_luma_ac,   0, sizeof(huff_tree_luma_ac));
+    memset(huff_tree_chroma_ac, 0, sizeof(huff_tree_chroma_ac));
+
+    // 生成huffman编码树
+    make_huffman_tree(default_ht_luma_dc_len,   default_ht_luma_dc,   HUFF_TREE_DC_LEN, huff_tree_luma_dc);
+    make_huffman_tree(default_ht_chroma_dc_len, default_ht_chroma_dc, HUFF_TREE_DC_LEN, huff_tree_chroma_dc);
+    make_huffman_tree(default_ht_luma_ac_len,   default_ht_luma_ac,   HUFF_TREE_AC_LEN, huff_tree_luma_ac);
+    make_huffman_tree(default_ht_chroma_ac_len, default_ht_chroma_ac, HUFF_TREE_AC_LEN, huff_tree_chroma_ac);
+
+    // 生成量化参数表
+    make_qt_table(qt, default_luma_table, luma_table);
+    make_qt_table(qt, default_chroma_table, chroma_table);
+    print_block_u8(luma_table);
+    print_block_u8(chroma_table);
 
     read_bmp_to_rgb888("test.bmp", &rgb_data, &w, &h);
-    dbg_rgb888_dump_ppm("out.ppm", rgb_data, w, h);
-    dbg_rgb888_dump_bmp("out.bmp", rgb_data, w, h);
+    // rgb888_dump_ppm("out.ppm", rgb_data, w, h);
+    fp_jpeg = fopen("output.jpeg", "wb");
+
+    // 写文件头
+    jpeg_write_u16(sw16(0xFFD8));
+
+    // 写JFIF
+    jfif.len = sw16(sizeof(JFIF_Header));
+    strcpy(jfif.identifier, "JFIF");
+    jfif.version = sw16(0x0102);
+    jfif.units = 0x01; // 0x00-unspecified 0x01-dots_per_inch 0x02-dots_per_cm
+    jfif.Hdensity = sw16(96);
+    jfif.Vdensity = sw16(96);
+    jfif.HthumbnailA = sw16(0);
+    jfif.VthumbnailA = sw16(0);
+    jpeg_write_u16(sw16(0xFFE0));
+    jpeg_write_file(&jfif, sizeof(JFIF_Header));
+
+    // 写量化表
+    jpeg_write_u16(sw16(0xFFDB));
+    jpeg_write_u16(sw16(2+1+64));
+    jpeg_write_u8(0x00); // AAAABBBB(bin), AAAA = 精度(0:8位，1:16位) BBBB = 量化表ID（最多4个）
+    jpeg_write_file(luma_table, sizeof(luma_table)); // 亮度量化表
+
+    jpeg_write_u16(sw16(0xFFDB));
+    jpeg_write_u16(sw16(2+1+64));
+    jpeg_write_u8(0x01); // AAAABBBB(bin), AAAA = 精度(0:8位，1:16位) BBBB = 量化表ID（最多4个）
+    jpeg_write_file(chroma_table, sizeof(chroma_table)); // 色度量化表
+
+    // TODO 写图像开始标记
+
+
+    // TODO 写huffman表
+
+
+    // TODO jpeg数据写入
+
+
 
     free(rgb_data);
+    fclose(fp_jpeg);
 
     return 0;
 }
